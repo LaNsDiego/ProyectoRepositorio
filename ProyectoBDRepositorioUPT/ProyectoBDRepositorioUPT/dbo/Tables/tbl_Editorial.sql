20ac3ca5abc2ca4e4a78eb255a8385aed046916f
Diego Rolando Layme Valeriano <dvansl@hotmail.com>
2018-08-25 15:41:39 -0500

    Subiendo Proyecto de Base de Datos para Repositorio UPT

---
+++ ProyectoBDRepositorioUPT/ProyectoBDRepositorioUPT/dbo/Tables/tbl_Editorial.sql
@@ -0,0 +1,12 @@
+﻿--CREATE DATABASE Repositorio;
+--use  Repositorio;
+
+--TABLA BASE
+CREATE TABLE tbl_Editorial (
+    Id_Editorial int IDENTITY PRIMARY KEY,
+	Nombre varchar(255) NOT NULL,
+	Direccion varchar(255) NOT NULL,
+	Estado int,
+	Eliminado int,
+    Fecha_Registro date
+);
