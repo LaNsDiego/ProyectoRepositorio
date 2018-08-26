883f732af1cd8759aab679aec3ac5c2ad602a9b5
Diego Rolando Layme Valeriano <dvansl@hotmail.com>
2018-08-25 19:11:48 -0500

    Use en table Editorial

--- ProyectoBDRepositorioUPT/ProyectoBDRepositorioUPT/Scripts/Script.tbl_Editorial.sql
+++ ProyectoBDRepositorioUPT/ProyectoBDRepositorioUPT/Scripts/Script.tbl_Editorial.sql
@@ -9,4 +9,5 @@ Plantilla de script posterior a la implementación
                SELECT * FROM [$(TableName)]					
 --------------------------------------------------------------------------------------
 */
+use Repositorio;
 INSERT INTO tbl_Editorial VALUES('Universidad Privada de Tacna','Av. Jorge Basadre Grohmann s/n Pocollay',1,0,'2010-05-10');
